e81ee2c7c5736d57d6677771b658a9ca28e2e0a9
Chester Hunt <chester.hunt@wedoresi.com>
2017-08-15 15:24:02 -0400

    Added unit tests for PanasonicCommandBuilder.

--- ICD.Connect.Cameras.Visca.Tests/PanasonicCommandBuilderTest.cs
+++ ICD.Connect.Cameras.Visca.Tests/PanasonicCommandBuilderTest.cs
@@ -14,30 +14,23 @@ namespace ICD.Connect.Cameras.Panasonic.Tests_NetStandard
             Assert.AreEqual("cgi-bin/aw_ptz?cmd=%23PTS5050&res=1", result);
         }
 
-        /*
-        /// <summary>
-        /// Moves the camera or zooms in, based on eCameraAction.
-        /// </summary>
-        /// <param name="Id"></param>
-        /// <param name="action"></param>
-        /// <param name="panSpeed"></param>
-        /// <param name="tiltSpeed"></param>
-        /// <param name="zoomSpeed"></param>
-        /// <returns></returns>
-        [TestCase(eCameraAction.Up, 48, 48, 48, "/cgi-bin/aw_ptz?cmd=#PTS5098&res=1")]
-        [TestCase(eCameraAction.Down, 48, 48, 48, "/cgi-bin/aw_ptz?cmd=#PTS5002&res=1")]
-        [TestCase(eCameraAction.Left, 48, 48, 48, "/cgi-bin/aw_ptz?cmd=#PTS0250&res=1")]
-        [TestCase(eCameraAction.Right, 48, 48, 48, "/cgi-bin/aw_ptz?cmd=#PTS9850&res=1")]
-        [TestCase(eCameraAction.ZoomIn, 48, 48, 48, "/cgi-bin/aw_ptz?cmd=#Z02&res=1")]
-        [TestCase(eCameraAction.ZoomOut, 48, 48, 48, "/cgi-bin/aw_ptz?cmd=#Z98&res=1")]
-        public void Move(eCameraAction action, int panSpeed, int tiltSpeed, int zoomSpeed, string expected)
+        [Test]
+        public void StopZoom()
         {
-            commandHandler.SetDefaultPanSpeed(panSpeed);
-            commandHandler.SetDefaultTiltSpeed(tiltSpeed);
-            commandHandler.SetDefaultZoomSpeed(zoomSpeed);
-            string result = commandHandler.Move(action);
+            string result = PanasonicCommandBuilder.StopZoom();
+            Assert.AreEqual("cgi-bin/aw_ptz?cmd=%23Z50&res=1", result);
+        }
+
+        [TestCase(eCameraAction.Up, "cgi-bin/aw_ptz?cmd=%23PTS5074&res=1")]
+        [TestCase(eCameraAction.Down, "cgi-bin/aw_ptz?cmd=%23PTS5026&res=1")]
+        [TestCase(eCameraAction.Left, "cgi-bin/aw_ptz?cmd=%23PTS2650&res=1")]
+        [TestCase(eCameraAction.Right, "cgi-bin/aw_ptz?cmd=%23PTS7450&res=1")]
+        [TestCase(eCameraAction.ZoomIn, "cgi-bin/aw_ptz?cmd=%23Z74&res=1")]
+        [TestCase(eCameraAction.ZoomOut, "cgi-bin/aw_ptz?cmd=%23Z26&res=1")]
+        public void Move(eCameraAction action, string expected)
+        {
+            string result = PanasonicCommandBuilder.Move(action);
             Assert.AreEqual(expected, result);
         }
-        */
     }
 }
