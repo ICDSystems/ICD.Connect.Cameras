63f8410e45d173c55a417c12d2ae5e74044c7136
Jack Kanarish <jack.kanarish@wedoresi.com>
2018-01-30 15:15:20 -0500

    Add some console commands

--- ICD.Connect.Cameras/Controls/PanTiltControl.cs
+++ ICD.Connect.Cameras/Controls/PanTiltControl.cs
@@ -40,5 +40,34 @@ namespace ICD.Connect.Cameras.Controls
 			Parent.PanTilt(eCameraPanTiltAction.Down);
 		}
 		#endregion
+
+		#region Console
+
+		/// <summary>
+		/// Gets the child console commands.
+		/// </summary>
+		/// <returns></returns>
+		public override IEnumerable<IConsoleCommand> GetConsoleCommands()
+		{
+			foreach (IConsoleCommand command in GetBaseConsoleCommands())
+				yield return command;
+
+			yield return new ConsoleCommand("Stop", "Sends the stop signal to the camera.", () => Stop());
+			yield return new ConsoleCommand("Up", "Sends the tilt up signal to the camera.", () => TiltUp());
+			yield return new ConsoleCommand("Down", "Sends the tilt down signal to the camera.", () => TiltDown());
+			yield return new ConsoleCommand("Left", "Sends the pan left signal to the camera.", () => PanLeft());
+			yield return new ConsoleCommand("Right", "Sends the pan right signal to the camera.", () => PanRight());
+		}
+
+		/// <summary>
+		/// Workaround for the "unverifiable code" warning.
+		/// </summary>
+		/// <returns></returns>
+		private IEnumerable<IConsoleCommand> GetBaseConsoleCommands()
+		{
+			return base.GetConsoleCommands();
+		}
+
+		#endregion
 	}
 }
--- ICD.Connect.Cameras/Controls/ZoomControl.cs
+++ ICD.Connect.Cameras/Controls/ZoomControl.cs
@@ -35,5 +35,32 @@ namespace ICD.Connect.Cameras.Controls
 			Parent.Zoom(eCameraZoomAction.ZoomOut);
 		}
 		#endregion
+
+		#region Console
+
+		/// <summary>
+		/// Gets the child console commands.
+		/// </summary>
+		/// <returns></returns>
+		public override IEnumerable<IConsoleCommand> GetConsoleCommands()
+		{
+			foreach (IConsoleCommand command in GetBaseConsoleCommands())
+				yield return command;
+
+			yield return new ConsoleCommand("Stop", "Sends the stop signal to the camera.", () => Stop());
+			yield return new ConsoleCommand("ZoomIn", "Sends the zoom in signal to the camera.", () => ZoomIn());
+			yield return new ConsoleCommand("ZoomOut", "Sends the zoom out signal to the camera.", () => ZoomOut());
+		}
+
+		/// <summary>
+		/// Workaround for the "unverifiable code" warning.
+		/// </summary>
+		/// <returns></returns>
+		private IEnumerable<IConsoleCommand> GetBaseConsoleCommands()
+		{
+			return base.GetConsoleCommands();
+		}
+
+		#endregion
 	}
 }
