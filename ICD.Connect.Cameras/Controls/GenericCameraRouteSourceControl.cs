9f2c3c1ea112a793150e0fc1b31b9712fb300aeb
Chris Cameron <chris.cameron@wedoresi.com>
2018-08-22 12:58:25 -0400

    perf: Significant improvement to route control performance

--- ICD.Connect.Cameras/Controls/GenericCameraRouteSourceControl.cs
+++ ICD.Connect.Cameras/Controls/GenericCameraRouteSourceControl.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using ICD.Common.Utils;
-using ICD.Common.Utils.Extensions;
 using ICD.Connect.Cameras.Devices;
 using ICD.Connect.Routing;
 using ICD.Connect.Routing.Connections;
@@ -14,8 +11,16 @@ namespace ICD.Connect.Cameras.Controls
 	public sealed class GenericCameraRouteSourceControl<TCameraDevice> : AbstractRouteSourceControl<TCameraDevice>
 		where TCameraDevice : ICameraDevice
 	{
+		/// <summary>
+		/// Raised when the device starts/stops actively transmitting on an output.
+		/// </summary>
 		public override event EventHandler<TransmissionStateEventArgs> OnActiveTransmissionStateChanged;
 
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="parent"></param>
+		/// <param name="id"></param>
 		public GenericCameraRouteSourceControl(TCameraDevice parent, int id)
 			: base(parent, id)
 		{
@@ -28,29 +33,47 @@ namespace ICD.Connect.Cameras.Controls
 			base.DisposeFinal(disposing);
 		}
 
+		/// <summary>
+		/// Returns true if the device is actively transmitting on the given output.
+		/// This is NOT the same as sending video, since some devices may send an
+		/// idle signal by default.
+		/// </summary>
+		/// <param name="output"></param>
+		/// <param name="type"></param>
+		/// <returns></returns>
 		public override bool GetActiveTransmissionState(int output, eConnectionType type)
 		{
-			if (EnumUtils.HasMultipleFlags(type))
-			{
-				return
-					EnumUtils.GetFlagsExceptNone(type)
-					         .Select(f => GetActiveTransmissionState(output, f))
-					         .Unanimous(false);
-			}
+			if (type != eConnectionType.Video)
+				throw new ArgumentOutOfRangeException("type");
 
-			if (output != 1)
-			{
-				string message = string.Format("{0} has no {1} output at address {2}", this, type, output);
-				throw new KeyNotFoundException(message);
-			}
+			if (output == 1)
+				return true;
 
-			switch (type)
-			{
-				case eConnectionType.Video:
-					return true;
-				default:
-					throw new ArgumentOutOfRangeException("type");
-			}
+			string message = string.Format("{0} has no {1} output at address {2}", this, type, output);
+			throw new KeyNotFoundException(message);
+		}
+
+		/// <summary>
+		/// Gets the output at the given address.
+		/// </summary>
+		/// <param name="output"></param>
+		/// <returns></returns>
+		public override ConnectorInfo GetOutput(int output)
+		{
+			if (!ContainsOutput(output))
+				throw new ArgumentOutOfRangeException("output");
+
+			return new ConnectorInfo(output, eConnectionType.Video);
+		}
+
+		/// <summary>
+		/// Returns true if the source contains an output at the given address.
+		/// </summary>
+		/// <param name="output"></param>
+		/// <returns></returns>
+		public override bool ContainsOutput(int output)
+		{
+			return output == 1;
 		}
 
 		public override IEnumerable<ConnectorInfo> GetOutputs()
