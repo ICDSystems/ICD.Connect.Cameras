fae30359a5232a9da59230e314ee6d33abf129f8
Chris Cameron <chris.cameron@wedoresi.com>
2020-01-03 10:28:28 -0500

    chore: Updating AssemblyInfo

--- ICD.Connect.Cameras/Properties/AssemblyInfo.cs
+++ ICD.Connect.Cameras/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Cameras")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Cameras")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
 [assembly: AssemblyVersion("4.2.0.0")]
