73b2d0d4beeaf8e0c18bcc63319f78539bd2d6b1
Chris Cameron <chris.cameron@wedoresi.com>
2021-01-06 16:45:04 -0500

    chore: Updating copyright year

--- ICD.Connect.Cameras.Mock/Properties/AssemblyInfo.cs
+++ ICD.Connect.Cameras.Mock/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Cameras.Mock")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Cameras.Mock")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2021")]
 [assembly: AssemblyVersion("5.0.2.0")]
--- ICD.Connect.Cameras.Vaddio/Properties/AssemblyInfo.cs
+++ ICD.Connect.Cameras.Vaddio/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Cameras.Vaddio")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Cameras.Vaddio")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2020")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2021")]
 [assembly: AssemblyVersion("5.0.2.0")]
