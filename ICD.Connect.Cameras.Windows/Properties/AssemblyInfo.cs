e822129e9c8ac60a05efd87799e8de6bb7fded17
Drew Tingen <drew.tingen@icdsystems.com>
2021-01-25 12:18:24 -0500

    chore: update changelog, increment assembly version

--- ICD.Connect.Cameras.Windows/Properties/AssemblyInfo.cs
+++ ICD.Connect.Cameras.Windows/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Cameras.Windows")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2021")]
-[assembly: AssemblyVersion("5.1.0.0")]
+[assembly: AssemblyVersion("5.1.1.0")]
