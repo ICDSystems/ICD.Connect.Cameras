5753b37ee31607a32a1051137805a6bf880600cf
Chris Cameron <chris.cameron@icdsystems.com>
2022-01-05 16:27:18 -0500

    chore: Updating AssemblyInfo year

--- ICD.Connect.Cameras.Panasonic/Properties/AssemblyInfo.cs
+++ ICD.Connect.Cameras.Panasonic/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.Cameras.Panasonic")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Cameras.Panasonic")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2021")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2022")]
 [assembly: AssemblyVersion("5.2.0.0")]
