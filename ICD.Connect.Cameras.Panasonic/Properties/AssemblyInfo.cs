10906a633bf046b399daa23b777fa8f9f0c861e9
Drew Tingen <drew.tingen@icdsystems.com>
2023-03-22 08:33:49 -0400

    chore: Update changelog, increment assembly patch version

--- ICD.Connect.Cameras.Panasonic/Properties/AssemblyInfo.cs
+++ ICD.Connect.Cameras.Panasonic/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Cameras.Panasonic")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2023")]
-[assembly: AssemblyVersion("5.2.1.0")]
+[assembly: AssemblyVersion("5.2.2.0")]
--- ICD.Connect.Cameras.Visca/Properties/AssemblyInfo.cs
+++ ICD.Connect.Cameras.Visca/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.Cameras.Visca")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2023")]
-[assembly: AssemblyVersion("5.2.1.0")]
+[assembly: AssemblyVersion("5.2.2.0")]
